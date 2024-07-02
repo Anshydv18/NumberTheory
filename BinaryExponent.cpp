1c2b2a7d78ff3e60fc97e486b5aa68c5a8a67419
Anshydv18 <anshgithub@gmail.com>
2024-07-02 13:24:45 +0530

    Binary Exponention

---
+++ BinaryExponent.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binpow(int a,int n){
+    if(n==0) return 1;
+     int ans = binpow(a,n/2);
+    if(n%2==0){
+        return ans*ans;
+    }
+    else{
+        return ans*ans*a;
+    }
+}
+int main(){
+    //binary exponentiation
+    int a,b; cin>>a>>b;
+    int res=1;
+    long long m = 1e9+7;
+    a=a%m;
+    while(b){
+        if(b&1)
+            res=res*a %m;
+        a=a*a%m;
+        b>>=1;
+    }
+    cout<<res<<endl;
+
+    cout<<binpow(2,5)<<endl;
+    
+}
